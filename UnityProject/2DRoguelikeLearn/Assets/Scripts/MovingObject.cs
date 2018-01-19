38ed5f5974c9b01f7b7578b4fd14e3d5ef443197
i-kiwi <i.kiwi@qq.com>
2018-01-19 17:53:32 +0800

    01191751
    XD

--- UnityProject/2DRoguelikeLearn/Assets/Scripts/MovingObject.cs
+++ UnityProject/2DRoguelikeLearn/Assets/Scripts/MovingObject.cs
@@ -11,6 +11,9 @@ public class MovingObject : MonoBehaviour {
 	private Rigidbody2D rb2D;
 	private float inverseMoveTime;
 
+    private bool isJump = false;
+    private bool isDoubleJump = false;
+
 
 	// Use this for initialization
     protected virtual void Start () {
@@ -19,58 +22,79 @@ public class MovingObject : MonoBehaviour {
 		inverseMoveTime = 1f / moveTime;
 	}
 	
-	protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    protected bool Move(int xDir, float yDir)
     {
 		Vector2 start = transform.position;
-		Vector2 end = start + new Vector2(xDir, yDir);
+		Vector2 moveEnd = start + new Vector2(xDir, 0);
+        Vector2 jumpEnd = start + new Vector2(0, yDir);
 
-		boxCollider.enabled = false;
+        //boxCollider.enabled = false;
 
-		hit = Physics2D.Linecast(start, end, blockingLayer);
+        //hit = Physics2D.Linecast(start, end, blockingLayer);
 
-		//boxCollider.enabled = true;
+        //boxCollider.enabled = true;
 
-		//if(hit.transform == null)
-		//{
-			StartCoroutine(SmoothMovement(end));
+        //if(hit.transform == null)
+        //{
+        //StartCoroutine(SmoothMovement(end));
+        SmoothMove(moveEnd);
+        Jump(jumpEnd);
+
 			return true;
 		//}
 		//return false;
 	}
+    protected void SmoothMove(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        if (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            //rb2D.MovePosition();
+
+            rb2D.MovePosition(newPosition);
+        }
+    }
+    protected void Jump(Vector3 end)
+    {
+        if (!isJump)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            //rb2D.MovePosition();
+
+            rb2D.MovePosition(newPosition);
+            isJump = true;
+        }
+    }
 
 	protected IEnumerator SmoothMovement(Vector3 end)
     {
-        Debug.Log("[end]" + end);
-		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-		//while(sqrRemainingDistance > float.Epsilon)
-		//{
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+		while(sqrRemainingDistance > float.Epsilon)
+		{
 			Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
-
+            //rb2D.MovePosition();
 			rb2D.MovePosition(newPosition);
 
-			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-			yield return null;
-		//}
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+		}
 	}
 
-	protected virtual void AttempMove<T> (int xDir, int yDir)
-		where T : Component
+    protected virtual void AttempMove (int xDir, float yDir)
 		{
-			RaycastHit2D hit;
-
-			bool canMove = Move(xDir, yDir, out hit);
-
-			if(hit.transform == null)
-				return;
-			
-			T hitComponent = hit.transform.GetComponent<T>();
-
-			// if(!canMove && hitComponent != null)
-				// OnCantMove(hitComponent);
+			bool canMove = Move(xDir, yDir);
 		}
 
-	 //protected abstract void OnCantMove <T> (T component)
-	 	//where T : Component;
+    //protected abstract void OnCantMove <T> (T component)
+    //where T : Component;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            isJump = false;
+            isDoubleJump = false;
+        }
+    }
 }
--- UnityProject/2DRoguelikeLearn/Assets/Scripts/Player.cs
+++ UnityProject/2DRoguelikeLearn/Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Player : MovingObject
 {
+    private float jumpForce = 2f;
+    private float gravity = 5f;
+
 	// Use this for initialization
     protected override void Start()
 	{
@@ -15,20 +18,37 @@ public class Player : MovingObject
         int horizontal = 0;     //Used to store the horizontal move direction.
         //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
         horizontal = (int)(Input.GetAxisRaw("Horizontal"));
-
-        if (horizontal != 0)
+        int jump = (int)(Input.GetAxisRaw("Jump"));
+        Debug.Log("[Jump]" + jump);
+        if (horizontal != 0 || jump != 0)
         {
+            if (horizontal > 0)
+            {
+                this.transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                this.transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+
             //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
             //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
-            AttempMove<Player>(horizontal, 0);
+            AttempMove(horizontal,jump * jumpForce);
+
+            //if (vertical > 0)
+            //{
+            //    vertical -= gravity * Time.deltaTime;
+            //}
         }
+
 	}
 
     //AttemptMove overrides the AttemptMove function in the base class MovingObject
     //AttemptMove takes a generic parameter T which for Player will be of the type Wall, it also takes integers for x and y direction to move in.
-    protected override void AttempMove<T>(int xDir, int yDir)
+    protected override void AttempMove(int xDir, float yDir)
     {
         //Call the AttemptMove method of the base class, passing in the component T (in this case Wall) and x and y direction to move.
-        base.AttempMove<T>(xDir, yDir);
+        base.AttempMove(xDir, yDir);
     }
 }
