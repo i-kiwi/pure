a040abb69dc7252da25d35fd334567501a716a38
i-kiwi <i.kiwi@qq.com>
2018-01-22 18:24:19 +0800

    01221824

---
+++ UnityProject/2DRoguelikeLearn/Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject hit = collision.collider.gameObject;
+        Debug.Log("hit tag" + hit.tag);
+        Health health = hit.GetComponent<Health>();
+
+        if(health != null)
+        {
+            health.TakeDamage(20);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ UnityProject/2DRoguelikeLearn/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public const float maxHealth = 100;
+    public float currentHealth = maxHealth;
+    public RectTransform hearthBar;
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("dead!");
+        }
+        hearthBar.sizeDelta = new Vector2(currentHealth, hearthBar.sizeDelta.y);
+    }
+}
