4a6e06fb316dc1d8e41133327acc0ab09e59e1fa
i-kiwi <i.kiwi@qq.com>
2018-01-22 09:23:08 +0800

    01220923

--- UnityProject/2DRoguelikeLearn/Assets/RockMan/RockMan_Controller.cs
+++ UnityProject/2DRoguelikeLearn/Assets/RockMan/RockMan_Controller.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class RockMan_Controller : MonoBehaviour
+public class RockMan_Controller : NetworkBehaviour
 {
 	Animator _anim;
 	public float MoveSpeed = 5f;
@@ -12,7 +13,10 @@ public class RockMan_Controller : MonoBehaviour
 	//public GameObject rocket;
 	//public ParticleSystem[] Smog;
 	//bool Attck_ing;
-	Vector3 Start_Scale;	
+	Vector3 Start_Scale;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
 	void Start ()
 	{
@@ -22,7 +26,10 @@ public class RockMan_Controller : MonoBehaviour
 	}
 
 	void Update ()
-    {                                                       
+    {             
+        //if(!isLocalPlayer){
+        //    return;
+        //}
         float xx = Input.GetAxisRaw("Horizontal");
         if (xx != 0) { 				
 			transform.localScale = new Vector3 (Start_Scale.x * xx, Start_Scale.y, Start_Scale.z);
@@ -38,6 +45,20 @@ public class RockMan_Controller : MonoBehaviour
             isJump = true;
             m_Rigidbody2D.AddForce(new Vector2(0f, JumpPower));
             _anim.SetTrigger("jump");                                           //Play ShotAnimation.													//Prevant change state until Finish this Motion.
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            _anim.SetBool("shoot", true);
+            var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+
+            // Destroy the bullet after 2 seconds
+            Destroy(bullet, 2.0f);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _anim.SetBool("shoot", false);
         }
 	}
     private void OnCollisionEnter2D(Collision2D collision)
